067076f61403242d375fe62205462bfa05562772
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2021-09-29 22:58:01 +0100

    Filter MsGraph request to requested app only

--- AzureAppSecretChecker/Services/MicrosoftGraphService.cs
+++ AzureAppSecretChecker/Services/MicrosoftGraphService.cs
@@ -20,6 +20,10 @@ namespace AzureAppSecretChecker.Services
             string clientId = azureAppCredential.ClientId;
             string clientSecret = azureAppCredential.ClientSecret;
 
+            if (tenantId.Length == 0 || tenantId == null) { throw new Exception("TenantId was empty or null. Please provide a valid TenantId"); }
+            if (clientId.Length == 0 || clientId == null) { throw new Exception("ClientId was empty or null. Please provide a valid ClientId"); }
+            if (clientSecret.Length == 0 || clientSecret == null) { throw new Exception("ClientSecret was empty or null. Please provide a valid ClientSecret"); }
+
             string[] scopes = new string[] { "https://graph.microsoft.com/.default" };
 
             try
@@ -34,32 +38,58 @@ namespace AzureAppSecretChecker.Services
                     tenantId, clientId, clientSecret, options);
 
                 var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
-                var allApplications = await graphClient.Applications.Request().GetAsync();
 
+                // there should only ever be one match for a specific appId, so top 1 is taken
+                // if the appId does not exist, then we should have an error by this point (in the clientSecretCredential creation)
+                // required permissions: https://docs.microsoft.com/en-us/graph/api/application-list?view=graph-rest-1.0#permissions
+                IGraphServiceApplicationsCollectionPage targetAppCollection = await graphClient.Applications
+                    .Request()
+                    .Header("ConsistencyLevel", "eventual")
+                    // filtering does not care for upper or lower case. Filter docs: https://docs.microsoft.com/en-us/graph/aad-advanced-queries#application-properties
+                    .Filter($"appId eq '{clientId}'")
+                    .Top(1)
+                    .GetAsync();
+
+
+                var targetApp = targetAppCollection.FirstOrDefault();
+                if (targetApp == null) { throw new Exception($"The connection to MS Graph was successful, but the requested clientId ({clientId}) was not found"); }
+
+
+                var passwordCredentials = targetApp.PasswordCredentials;
                 List<AzureAppSecretInfo> appSecrets = new List<AzureAppSecretInfo>();
-
-                foreach (Application app in allApplications)
+                if (passwordCredentials.Any())
                 {
-                    var passwordCredentials = app.PasswordCredentials;
-                    if (app.AppId == clientId && passwordCredentials.Any())
+                    // there can be more than one credential in each app
+                    foreach (PasswordCredential passwordCredential in passwordCredentials)
                     {
-                        // there can be more than one credential in each app
-                        foreach (PasswordCredential passwordCredential in passwordCredentials)
+                        AzureAppSecretInfo azureAppSecretInfo = new AzureAppSecretInfo
                         {
-                            string stringEndDateTime = passwordCredential.EndDateTime.HasValue ? passwordCredential.EndDateTime.Value.ToString() : "";
+                            ApplicationId = targetApp.AppId,
+                            DisplayName = passwordCredential.DisplayName, // descirption
+                            Domain = targetApp.DisplayName,
+                            EndDateTime = passwordCredential.EndDateTime.ToString(),
+                            ObjectId = targetApp.Id,
+                            SecretHint = passwordCredential.Hint, // first 3 characters of the secret
+                            TenantId = tenantId
+                        };
 
-                            appSecrets.Add(new AzureAppSecretInfo
-                            {
-                                ApplicationId = app.AppId,
-                                DisplayName = passwordCredential.DisplayName,
-                                Domain = app.PublisherDomain,
-                                EndDateTime = stringEndDateTime,
-                                ObjectId = app.Id,
-                                SecretHint = passwordCredential.Hint,
-                                TenantId = tenantId
-                            });
-                        }
+                        appSecrets.Add(azureAppSecretInfo);
                     }
+                }
+                else
+                {
+                    AzureAppSecretInfo azureAppWithoutSecrets = new AzureAppSecretInfo
+                    {
+                        ApplicationId = targetApp.AppId,
+                        DisplayName = "No app secrets found", // descirption
+                        Domain = targetApp.DisplayName,
+                        EndDateTime = "No app secrets found",
+                        ObjectId = targetApp.Id,
+                        SecretHint = "No app secrets found", // first 3 characters of the secret
+                        TenantId = tenantId
+                    };
+
+                    appSecrets.Add(azureAppWithoutSecrets);
                 }
 
                 return appSecrets;
